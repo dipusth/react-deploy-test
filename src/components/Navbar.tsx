512c8d09edc8bf0ef42802705a6a57e8b73e6c90
dipu <deepshrestha365@gmail.com>
2025-08-26 17:04:21 +0545

    intial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+export function Navbar() {
+  return (
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Blog</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+  );
+}
