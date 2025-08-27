d9cb04236a8e98c6223f457e5cffd9a971dc76f9
dipu <deepshrestha365@gmail.com>
2025-08-27 10:04:20 +0545

    updating path with alias@

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Navbar } from "./components/common/Navbar";
-import { Button } from "./components/ui/button";
+import { Navbar } from "@/components/common/Navbar";
+import { Button } from "@/components/ui/button";
 
 function App() {
   const [count, setCount] = useState(0);
